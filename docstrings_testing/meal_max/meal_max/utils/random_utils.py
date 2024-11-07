28e1b703d3f45f0bdfa3ec7a467f218a415069f4
Jerry <liuj847@bu.edu>
2024-11-06 22:33:34 -0500

    Finised docstring for random_utils.py

--- docstrings_testing/meal_max/meal_max/utils/random_utils.py
+++ docstrings_testing/meal_max/meal_max/utils/random_utils.py
@@ -8,6 +8,20 @@ configure_logger(logger)
 
 
 def get_random() -> float:
+    """
+    Fetches a random deciaml number from random.org with two decimal places
+
+    The function sends a GET request to random.org to retrieve a single random 
+    decimal fraction formatted to two decimal places.The response is parsed and converted to a float. If the request
+    is unsuccessful or if the response cannot be parsed as a float, an execption is raised.
+
+    Raises:
+        ValueError: if the response from random.org is not a valid float
+        RuntimeError: if the request to the random.org fails or times out
+
+    Returns:
+        float: A random decimal number between 0 and 1 with two decimal places
+    """
     url = "https://www.random.org/decimal-fractions/?num=1&dec=2&col=1&format=plain&rnd=new"
 
     try:
